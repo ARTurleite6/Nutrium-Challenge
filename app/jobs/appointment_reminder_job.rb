e42d0dc1891ddbadef69b98abc7a16bc41f17e8b
ARTurleite6 <arturleite164@gmail.com>
2025-08-02 14:59:10 +0100

    fix rubocop

--- app/jobs/appointment_reminder_job.rb
+++
@@ -1,9 +0,0 @@
-class AppointmentReminderJob < ApplicationJob
-  queue_as :reminders
-
-  def perform(appointment_id)
-    appointment = Appointment.find(appointment_id)
-
-    return unless appointment.
-  end
-end
